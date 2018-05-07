7dfbc6618b5cf1c8352b611fda07cb6319e5dda1
Jim <jinhengyu666@gmail.com>
2018-05-07 22:47:28 +0800

    修复了部分bug

--- flappy-ball/main.js
+++ flappy-ball/main.js
@@ -1,61 +1,67 @@
+const B_Array = [''];
+//画布宽高
+const cw = 1000;
+const ch = 700;
+const B_width = 70;
+const B_top = 100;
+const B_x = [];
+const B_kind = [];
+const nowTime = new Date();
+const btn = document.getElementById('btn');
+const score = document.querySelector('#score');     //分数
 
-var y = 400;
-var g = 0;
-var B_Array = [''];
-var WINDOW_WIDTH = 1000;
-var WINDOW_HEIGHT = 700;
-var B_width = 70;
-var B_top = 100;
-var bbb = 0;
-bbb = WINDOW_WIDTH - B_width;
-var B_x = [];
-var B_kind = [];
-var nowTime = new Date();
-var nowsecond = nowTime.getTime();
-var btn = document.getElementById('btn');
-var p = document.getElementsByTagName('p')[0];
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext("2d");
 
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext("2d");
+let y = 400;
+let g = 0;
+let bbb = cw - B_width;
+let nowsecond = nowTime.getTime();
+let isPaused = true;
 
-canvas.width = WINDOW_WIDTH;
-canvas.height = WINDOW_HEIGHT;
+canvas.width = cw;
+canvas.height = ch;
+// canvas.focus();
 
-var gameStrat = function() {
-    if (btn.innerHTML == "重玩") { window.location.href = window.location.href; }
+context.fillStyle = 'rgba(0,0,0,0.6)';
+context.fillRect(0, 0, cw, ch);
+
+const gameStrat = function () {
+    if (btn.innerHTML === "重玩") {
+        // window.location.href = window.location.href;
+        location.reload();
+    }
     btn.style.display = "none";
-    ball = setInterval(function() {
+    canvas.onmousedown = document.onkeydown = flap;
+    ball = setInterval(function () {
         render(context);
     }, 7)
-}
+};
 
-function mouseDown() {
+function flap() {
     if (g = 3) g = -g * 5
 }
-document.onmousedown = mouseDown;
-
-function keyDown() {
-    if (g = 3) g = -g * 5
-}
-document.onkeydown = keyDown;
+canvas.onmousedown = document.onkeydown = gameStrat;
 
 function render(cxt) {
 
-    cxt.clearRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-    if (g < 3) { g++; }
+    cxt.clearRect(0, 0, cw, ch);
+    if (g < 3) {
+        g++;
+    }
     y += g; //  球掉落速度
 
     renderBall(cxt);
-    renderBarrier(cxt)
+    renderBarrier(cxt);
     barrierAdd();
     collisionTest();
 
 }
 
 function barrierAdd() {
-    var nextTime = new Date();
-    var nextsecond = nextTime.getTime();
-    var diff = nextsecond - nowsecond;
+    const nextTime = new Date();
+    const nextsecond = nextTime.getTime();
+    const diff = nextsecond - nowsecond;
     if (diff >= 1500) { //障碍物出现间隔
         B_x.push(bbb);
         nowsecond = nextsecond;
@@ -81,35 +87,41 @@ function renderBarrier(cxt) {
 
     cxt.fillStyle = "rgb(50,150,250)";
 
-    for (var i = 0; i < B_Array.length; i++) {
+    for (let i = 0; i < B_Array.length; i++) {
         B_x[i] -= 3; //  障碍物移动速度
         cxt.fillRect(B_x[i], 0, 80, B_top * B_kind[i]);
-        cxt.fillRect(B_x[i], B_top * B_kind[i] + 200, 80, WINDOW_HEIGHT - B_top * B_kind[i] - 200);
+        cxt.fillRect(B_x[i], B_top * B_kind[i] + 200, 80, ch - B_top * B_kind[i] - 200);
     }
 }
 
+//碰撞检测
 function collisionTest() {
-    for (var i = 0; i < B_Array.length; i++) {
+    let collided = false;
+    for (let i = 0; i < B_Array.length; i++) {
         if (B_x[i] > 40 && B_x[i] < 120) {
             if (y < B_top * B_kind[i] + 28 || y > B_top * B_kind[i] + 200 - 28) {
-                btn.style.display = "block";
-                btn.innerHTML = "重玩";
-                clearInterval(ball);
-            } else { p.innerHTML = i; }
+                collided = true;
+            } else {
+                score.innerHTML = i;
+            }
         }
         if (B_x[i] > 10 && B_x[i] < 40) {
             if (y < B_top * B_kind[i] + 15 || y > B_top * B_kind[i] + 200 - 15) {
-                btn.style.display = "block";
-                btn.innerHTML = "重玩";
-                clearInterval(ball);
+                collided = true;
             }
         }
         if (B_x[i] > 120 && B_x[i] < 150) {
             if (y < B_top * B_kind[i] + 15 || y > B_top * B_kind[i] + 200 - 15) {
-                btn.style.display = "block";
-                btn.innerHTML = "重玩";
-                clearInterval(ball);
+                collided = true;
             }
         }
+    }
+    if (collided) {
+        context.fillStyle = 'rgba(0,0,0,0.6)';
+        context.fillRect(0, 0, cw, ch);
+        btn.style.display = "block";
+        btn.innerHTML = "重玩";
+        clearInterval(ball);
+        canvas.onmousedown = document.onkeydown = gameStrat;
     }
 }
